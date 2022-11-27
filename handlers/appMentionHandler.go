ce07a8bc07bd9ccbaabed876d6d9fea78f3b9a62
William Bratz <williambratz615@gmail.com>
2022-11-27 15:44:31 -0700

    Updates structure and func names

--- handlers/appMentionHandler.go
+++ handlers/appMentionHandler.go
@@ -2,14 +2,11 @@ package handlers
 
 import (
 	"fmt"
-	"prolice-slack-bot/posts"
 	"prolice-slack-bot/types"
 	"strings"
-	"time"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
-	"mvdan.cc/xurls/v2"
 )
 
 // HandleAppMentionEventToBot is used to take care of the AppMentionEvent when the bot is mentioned
@@ -39,44 +36,21 @@ func HandleAppMentionEventToBot(event *slackevents.AppMentionEvent, client *slac
 	// 		Value: user.Name,
 	// 	},
 	// }
-	if strings.Contains(text, "https://carvanadev.visualstudio.com/carvana.underwriting/") && strings.Contains(text, "pullrequest") {
-
-		xurlsStrict := xurls.Strict()
-		prFromText := xurlsStrict.FindAllString(text, -1)
-		pr := types.PullRequest{User: fmt.Sprintf("%s %s", user.Profile.FirstName, user.Profile.LastName), PrUrl: prFromText[len(prFromText)-1], Posted: time.Now()}
-
-		*currentPRs = append(*currentPRs, pr)
-
-		attachment.Text = fmt.Sprintf("PR: %s added", pr.PrUrl)
-		attachment.Color = "#4af030"
-
-		posts.PostMessageWithErrorLogging(client.PostMessage, event.Channel, slack.MsgOptionAttachments(attachment))
-
-		client.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
-	} else if strings.Contains(text, "list prs") {
-		// Send a message to the user
+	if strings.Contains(text, "list prs") {
 
 		for _, pr := range *currentPRs {
 			//use attachment.Fields here
 			attachment.Text = fmt.Sprintf("\nUrl: %s", pr.PrUrl)
 			attachment.Text += fmt.Sprintf("\nAuthor: %s", pr.User)
 			attachment.Text += fmt.Sprintf("\nPosted Date: %s", pr.Posted)
-
-			client.PostEphemeral(event.Channel, user.ID, slack.MsgOptionAttachments(attachment))
 		}
-
-		// attachment.Pretext = "How can I be of service"
-		attachment.Color = "#4af030"
-	} else if strings.Contains(text, "trigger") {
-
-		attachment.Text = "triggered"
-		posts.PostMessageWithErrorLogging(client.PostMessage, event.Channel, slack.MsgOptionAttachments(attachment))
 	} else {
-		// Send a message to the user
-		attachment.Text = fmt.Sprintf("I am good. How are you %s?", user.Name)
+		attachment.Text = fmt.Sprintf("Sorry %s, I do not know how to handle that request", user.Name)
 		// attachment.Pretext = "How can I be of service"
-		attachment.Color = "#4af030"
 	}
+
+	attachment.Color = "#4af030"
+	client.PostEphemeral(event.Channel, user.ID, slack.MsgOptionAttachments(attachment))
 
 	return nil
 }
--- handlers/channelMessageHandler.go
+++ handlers/channelMessageHandler.go
@@ -1,25 +1,27 @@
 package handlers
 
 import (
+	"fmt"
 	"prolice-slack-bot/posts"
 	"prolice-slack-bot/types"
+	"strings"
 	"time"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+	"mvdan.cc/xurls/v2"
 )
 
 func HandleMessageEvent(event *slackevents.MessageEvent, client *slack.Client, currentPRs *[]types.PullRequest) error {
 
 	// Grab the user name based on the ID of the one who mentioned the bot
-	_, err := client.GetUserInfo(event.User)
+	user, err := client.GetUserInfo(event.User)
 	if err != nil {
 		return err
 	}
-	// Check if the user said Hello to the bot
-	//text := strings.ToLower(event.Text)
 
-	// Create the attachment and assigned based on the message
+	text := strings.ToLower(event.Text)
+
 	attachment := slack.Attachment{}
 	// Add Some default context like user who mentioned the bot
 	// attachment.Fields = []slack.AttachmentField{
@@ -32,9 +34,25 @@ func HandleMessageEvent(event *slackevents.MessageEvent, client *slack.Client, c
 	// 	},
 	// }
 
-	attachment.Text = "Its Midnight"
+	if strings.Contains(text, "https://carvanadev.visualstudio.com/carvana.underwriting/") &&
+		strings.Contains(text, "pullrequest") {
 
-	// becareful for infinate loops here (bot triggering itself)
+		xurlsStrict := xurls.Strict()
+		prFromText := xurlsStrict.FindAllString(text, -1)
+		pr := types.PullRequest{User: fmt.Sprintf("%s %s", user.Profile.FirstName, user.Profile.LastName),
+			PrUrl:  prFromText[len(prFromText)-1],
+			Posted: time.Now()}
+
+		*currentPRs = append(*currentPRs, pr)
+
+		attachment.Text = fmt.Sprintf("PR: %s added", pr.PrUrl)
+		attachment.Color = "#4af030"
+
+		posts.PostMessageWithErrorLogging(client.PostMessage, event.Channel, slack.MsgOptionAttachments(attachment))
+	}
+
+	//attachment.Text = "Its Midnight"
+
 	if time.Now().Hour() == 0 {
 		posts.PostMessageWithErrorLogging(client.PostMessage, event.Channel, slack.MsgOptionAttachments(attachment))
 	}
